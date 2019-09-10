52639aecb0d2a055ae7f3c8ca0fc082f39196196
Jake Esveld <thejakeesveld@gmail.com>
2019-09-10 13:31:13 -0700

    clean up recursive sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,29 +1,22 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    print(f"Array A {arrA}   Array B {arrB}")
+    merged_arr = []
     # TO-DO
     while len(arrA) != 0 and len(arrB) != 0:
         if arrA[0] < arrB[0]:
-            merged_arr.append(arrA[0])
-            merged_arr.pop(0)
+            merged_arr.append(arrA[0])           
             arrA.pop(0)
         else:
-            merged_arr.append(arrB[0])
-            merged_arr.pop(0)
+            merged_arr.append(arrB[0])           
             arrB.pop(0)
     if len(arrA) != 0:
         for i in range(0, len(arrA)):
             merged_arr.append(arrA[i])
-            merged_arr.pop(0)
-
+            
     if len(arrB) != 0:
         for i in range(0, len(arrB)):
-            merged_arr.append(arrB[i])
-            merged_arr.pop(0)
-            
-    
+            merged_arr.append(arrB[i])         
+               
     return merged_arr
 
 
@@ -38,20 +31,16 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-    
     if len(arr) <= 1 :
         return arr
 
-    
     left = arr[0: len(arr) // 2]
     right = arr[len(arr) // 2: len(arr)]
     sortedLeft = merge_sort(left)
     sortedRight = merge_sort(right)
 
-    print(f"Left: {sortedLeft}     Right: {sortedRight}")
     arr = merge(sortedLeft, sortedRight)   
     
-
     return arr
 
 
